10adcd5418b56a8a1f3f179837de4da4f28e6f9e
Anthony Guimard <bonjour@anthonyguimard.fr>
2017-11-30 23:08:29 -0500

    Add cron job.
    Add cron-job and add a lastupdate route in order
    to ensure that update is made.
    Timestamp file is now write in /tmp directory due to
    EACCESS errors.

--- johnny-core/index.js
+++ johnny-core/index.js
@@ -1,8 +1,53 @@
 const wtf = require('wtf_wikipedia');
 const express = require('express');
+const CronJob = require('cron').CronJob;
 const fs = require('fs');
-const filename = __dirname + "/timestamp";
-const timeRange = 1000*60*10; // 10 minutes
+const filename = "/tmp/timestamp";
+
+const getWikipediaInfo = () => {
+  wtf.from_api('Johnny Hallyday', 'en', (markup) => {
+    const response = wtf.parse(markup);
+
+    try {
+      const { infoboxes: [{ data }] } = response;
+      const death = !!data.death_date;
+
+      const jsonData = {
+        "timestamp": Date.now(),
+        "death": death
+      };
+
+      // set new file system
+      try {
+        fs.writeFileSync(filename, JSON.stringify(jsonData), function(err){
+          if(err) {
+            console.log(err);
+          }
+        });
+
+        console.log('Last write file at ', new Date());
+      } catch(err) {
+        console.log(err);
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+// Define a cron job
+// every 10 minutes he try to reach wikipedia page
+// and when it succeed it will populate a timestamp file
+const job = new CronJob({
+  cronTime: '*/10 * * * *',
+  onTick: function() {
+    getWikipediaInfo();
+  },
+  start: true,
+  timeZone: 'America/New_York',
+  runOnInit: true,
+});
 
 const app = express();
 
@@ -10,46 +55,23 @@ app.get('/isjohnnydead', (req, res) => {
 
   // read cached file system
   fs.readFile(filename, 'utf8', function (err,data) {
-
-    var retrieveInfo = true;
-  	if(err || !data) fs.openSync(filename, 'w');
-    else if(Date.now() - JSON.parse(data).timestamp < timeRange) {
-
-	  const death = JSON.parse(data).death;
+  	const death = JSON.parse(data).death;
 	  retrieveInfo = false;
 
 	  res.json({
-   		isDead: death,
-      });
-	}
-
-   // retrieve data from wiki
-  if(retrieveInfo) {
-
-	wtf.from_api('Johnny Hallyday', 'en', (markup) => {
-      const response = wtf.parse(markup);
-	  const { infoboxes: [{ data }] } = response;
-      const death = !!data.death_date;
-
-	  const jsonData = {
-	    "timestamp": Date.now(),
-		"death": death
-	  };
-
-	  // set new file system
-	  fs.writeFileSync(filename, JSON.stringify(jsonData), function(err){
-	    if(err) console.log(err);
-	  });
-	  
-      res.json({
-   	    isDead: death,
-      });
-
+      isDead: death,
     });
-  } 
-  
- });
+  });
+});
 
+app.get('/lastupdate', (req, res) => {
+  fs.readFile(filename, 'utf8', function (err,data) {
+  	const timestamp = JSON.parse(data).timestamp;
+
+	  res.json({
+      lastUpdate: timestamp,
+    });
+  });
 });
 
 app.listen(4000, () => console.log('Johnny listening on port 4000!')); // eslint-disable-line
