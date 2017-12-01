ce71e7c3b8d471b2875973f9a7fdf7ede336e3d0
Anthony Guimard <bonjour@anthonyguimard.fr>
2017-11-30 23:36:46 -0500

    Update React and add title and images.

--- johnny-front/pages/index.js
+++ johnny-front/pages/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import Head from 'next/head';
 import 'isomorphic-unfetch';
 
 /* eslint-disable */
+const Fragment = React.Fragment;
 
 export default class Index extends React.Component {
   static async getInitialProps() {
-    const res = await fetch('http://localhost:4000/isjohnnydead');
+    const res = await fetch('https://api.johnnyestilmort.com/isjohnnydead');
     const { isDead } = await res.json();
     return { isDead };
   }
   render() {
     const { isDead } = this.props.isDead;
+    const style = {
+      title: {
+        textAlign: 'center',
+        margin: '1em',
+        lineHeight: '1.4em'
+      },
+      image: {
+        display: 'block',
+        margin: '1em auto',
+        width: 'auto',
+        maxWidth: '100%'
+      }
+    };
+
     return (
-      <div>
-        <h1>Is Johnny dead ? {isDead ? 'Yep' : 'nop'}</h1>
-      </div>
+      <Fragment>
+        <Head>
+          <title>Johnny est-il mort ? - {isDead ? 'Yep' : 'nop'}</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <img style={style.image} src={isDead ? '/static/images/johnny-2.jpg' : '/static/images/johnny.gif'} />
+        <h1 style={style.title}>Johnny est-il mort ? {isDead ? "Yep :'(" : "Nop"}</h1>
+      </Fragment>
     );
   }
 }
